c8762660330d5fcfc562c033ce0c9243ca416862
CieranNicholas <47529890+CieranNicholas@users.noreply.github.com>
2024-10-30 23:08:16 +0000

    feat: home page UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,5 @@
+import PodcastCard from '@/components/podcast-card';
+import { podcastData } from '@/constants';
 import React from 'react';
 
 function Home() {
@@ -5,6 +7,11 @@ function Home() {
 		<div className='mt-9 flex flex-col gap-9'>
 			<section className='flex flex-col gap-5'>
 				<h1 className='text-20 font-bold text-white-1'>Trending Podcasts</h1>
+				<div className='podcast_grid'>
+					{podcastData.map(({ id, title, description, imgURL }) => (
+						<PodcastCard key={id} id={id} title={title} description={description} imgURL={imgURL} />
+					))}
+				</div>
 			</section>
 		</div>
 	);
